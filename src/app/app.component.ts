ae4d54082d151d2974c4ffb74f7940e038fb5fee
Ante <ante.adamovic93@gmail.com>
2017-09-07 23:31:43 +0200

    Initial...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('username') usernameField: ElementRef;
+  @ViewChild('password') passwordField: ElementRef;
+  public usernameWarning: String;
+  public passwordWarning: String;
+
+  constructor() { }
+
+  public validateAndLogin(): void {
+    this.clearWarningText();
+    const username = this.usernameField.nativeElement.value;
+    const password = this.passwordField.nativeElement.value;
+
+    console.log(this);
+
+    if (!this.validateUsername()) {
+      this.usernameWarning = "Username must be at least 8 characters long and have 1 capital letter.";
+    } 
+    
+    if (!this.validatePassword()) {
+      this.passwordWarning = "Passowrd must be at least 8 characters long and have 1 number.";
+      return;
+    }
+
+    // Do login
+
+  }
+
+  // Clears warnings from both input fields
+  private clearWarningText(): void {
+    this.usernameWarning = "";
+    this.passwordWarning = "";
+  }
+
+  private validateUsername(): void {
+    const regex = /^(?=.{8,20}$)[a-zA-Z0-9._]/;
+    const username = this.usernameField.nativeElement.value;
+    
+    const result = regex.test(username);
+    if (!result) {
+      this.usernameWarning = "Username doesn't meet the criteria";
+    } else {
+      this.usernameWarning = "";
+    }
+
+    // return result;
+  }
+
+  private validatePassword(): boolean {
+    const password = this.passwordField.nativeElement.value;
+    console.log(password);
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { MdButtonModule, MdCheckboxModule, MdCardModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdCardModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
